611a2d054b8b6708326247334fc6b8868b46c1d4
lijw7 <15191050282@163.com>
2026-09-16 09:56:41 +0800

    [das] style(pallas): fix code indentation

--- jax/_src/pallas/pallas_call.py
+++ jax/_src/pallas/pallas_call.py
@@ -1190,8 +1190,8 @@ def _pallas_call_lowering(
 
     if is_rocm and backend is mosaic_gpu_backend:
       raise ValueError(
-        "Mosaic GPU does not yet support HCU/ROCm devices. "
-        "Use ``compiler_params=pltriton.CompilerParams()`` for ROCm."
+          "Mosaic GPU does not yet support HCU/ROCm devices. "
+          "Use ``compiler_params=pltriton.CompilerParams()`` for ROCm."
       )
 
     return backend.pallas_call_lowering(
